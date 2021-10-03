06d9aaa5bd296af68ba50df66df72c960077b7bd
jesiel364 <82860253+jesiel364@users.noreply.github.com>
2021-10-02 21:35:17 -0400

    Add files via upload

---
+++ index.html
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Task App</title>
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+            rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="estilo.css">
+</head>
+<body onload="ao_carregar()">
+	<header>
+		<h1>Agendador de Tarefas</h1>
+	</header>
+
+	<section>
+		<nav id="nav">
+			<input id='tarefatxt' type="text" name="" placeholder="Digite aqui...">
+			<button id="bt_add" onclick="add_task()"><span id="add-bt" class="material-icons md-24">add_task</span></button>
+			<button id="remove-bt" onclick="rem_task()"><span id="delete" class="material-icons md-24">delete</span></button>
+			<button id="menu"><span id="menu-bt" class="material-icons md-24">more_vert</span></button>
+		</nav>
+
+		<div id="res">
+		</div>
+		<div></div>
+	</section>
+
+	<footer>Instagram: @jesiel.py</footer>
+	<script src="script.js"></script>
+
+</body>
+</html>
--- script.js
+++ script.js
@@ -1,12 +1,12 @@
+// Variaveis globais
 var res = document.querySelector('div#res')
 var c = 1
 var b = 3
-
 var nav = document.querySelector('nav#nav')
 var remove_bt = document.querySelector('button#remove-bt')
 
-
-function ao_carregar () {
+// Ao iniciar irá mostrar "Sem tarefas"
+function ao_carregar () { 
 	
 	// while(c <= b){
 	// 	res.innerHTML += `<p id="${c}">&#9989;${c} passo concluido!</p>`
@@ -25,38 +25,86 @@ function ao_carregar () {
 }
 
 
-
+// Adiciona uma tarefa
 function add_task () {
 	var tarefa = document.querySelector('input#tarefatxt').value
-	if (b > 6){
+	if (b > 6){ 		//Se ultrapassar 3 tarefas ativa o scroll vertical
 			res.setAttribute('style', 'overflow-y: scroll;')
 		}else{
 			res.removeAttribute('style', 'overflow-y: scroll;')
 		}
-	b = 7
-	res.innerHTML += `<p id="${c}">&#9989;${tarefa}</p>`
-	c++
 
+	b = 7
+
+	// Div tarefa
+	var task = document.createElement('div')
+	task.setAttribute('id', `tarefa_${c}`)
+	task.setAttribute('class', `tarefa_area`)
+
+	// marcador
+	var mark = document.createElement('button')
+	mark.setAttribute('id', 'check_circle')
+	mark.innerHTML = '<span class="material-icons md-24"/>check_circle</span>'
+
+	// Nome da tarefa
+	var task_name = document.createElement('p')
+	task_name.setAttribute('id', `${c}`)
+	task_name.innerText = tarefa
+	task_name.appendChild(mark)
+
+	//	Descrição da tarefa
+
+	// butão da tarefa
+	// var task_bt = document.createElement('button')
+	// task_bt.setAttribute('class', 'task_bt')
+	// task_bt.setAttribute('onclick', 'auto_delete()')
+	// task_bt.setAttribute('id', `tarefa_${c}`)
+	// task_bt.innerHTML = "<span class='material-icons md-24'>delete</span>"
+	//task.appendChild(task_bt)
+	
+	task.appendChild(task_name)
+	res.appendChild(task)
+	task.innerHTML += '<button id="menu"><span id="menu-bt" class="material-icons md-24">delete</span></button>'
+
+
+	//	Se exceder o número de caracteres
+	var tamanho_da_tarefa = tarefa.length
+	// alert(tamanho_da_tarefa)
+	if (tamanho_da_tarefa > 23){
+		task.setAttribute('style', "text-indent = '3ch'")
+	}
+	
+	
+	c++													//	Itera o contador
+
+// Se o numero de tarefas for diferente de zero remove a mensagem "Sem tarefas" e mostra o botão "Remover Tarefa"
 	if(c != 0){
 		p1 = document.querySelector('p#p1')
 		res.removeChild(p1)}
 		nav.appendChild(remove_bt)
-
-
 }
 
+// Remove a ultima tarefa
 function rem_task () {;
-	var p = document.getElementById(c-1)
+	var task = document.getElementById(`tarefa_${c-1}`)
 	if (b > 6){
 			res.setAttribute('style', 'overflow-y: scroll;')
 		}else{res.removeAttribute('style', 'overflow-y: scroll;')}
-	res.removeChild(p)
-	c--
-	while(c == 1){
+	res.removeChild(task)		//	Remove a ultima tarefa "p"
+	c--						//	Diminui o contador
 	
-	res.innerHTML = `<p id="p${c}">Sem tarefas</p>`
+	while(c == 1){			//	Enquanto não tiver nenhuma tarefa mostra a mensagem "Sem tarefas" e remover o de "Remover Tarefa"		
+	res.innerHTML = `<p id="p${c}">Sem tarefas</p>`		
 	nav.removeChild(remove_bt)
-
 	break;
 }
+}
+
+function auto_delete (argument) {
+	
+	// console.log(c)
+	bt = document.querySelector('button#')
+	var self_task = document.getElementById(task_name)
+	res.removeChild(self_task)
+	c--
 }
